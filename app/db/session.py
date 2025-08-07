f4ac52ed8f113a51ce4ef3a1bef77fd7f48127e5
adgator101 <aadityathapa977@gmail.com>
2025-08-07 09:50:39 +0545

    feat(yolo_service): update detection output to return highest confidence detection and rename class key

--- app/db/session.py
+++ app/db/session.py
@@ -14,12 +14,12 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-DATABASE_URL = (
-    f"mssql+pyodbc://{DB_USER}:{urllib.parse.quote_plus(DB_PASSWORD)}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server"
-)
+# DATABASE_URL = (
+#     f"mssql+pyodbc://{DB_USER}:{urllib.parse.quote_plus(DB_PASSWORD)}"
+#     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server"
+# )
 
 engine = create_engine(
     DATABASE_URL,
